2cbe2a616087bd11f35849e82714547156579aa1
Puneet Mishra <puneetm@codeaurora.org>
2016-01-08 14:17:43 +0000

    NFC: NULL Pointer and mutex checks
    Several potential security issues were detected in
    the JNI of NfcService.
    
    Added NULL pointer checks and ensured mutex is
    also unlocked in error case.
    
    Change-Id: If11cb58368364166ce5bf81b1f872ffb06c741d0
    CRs-Fixed: 959662

--- nci/jni/SecureElement.cpp
+++ nci/jni/SecureElement.cpp
@@ -1,23 +1,12 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 /******************************************************************************
  *
- *  The original Work has been changed by NXP Semiconductors.
+ *  Copyright (c) 2016, The Linux Foundation. All rights reserved.
+ *  Not a Contribution.
  *
  *  Copyright (C) 2015 NXP Semiconductors
+ *  The original Work has been changed by NXP Semiconductors.
+ *
+ *  Copyright (C) 2012 The Android Open Source Project
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -1746,6 +1735,7 @@ void SecureElement::notifyEEReaderEvent (int evt, int data)
                     if (e == NULL)
                     {
                         ALOGE ("%s: jni env is null", fn);
+                        mMutex.unlock();
                         return;
                     }
                     sSwpReaderTimer.kill();
