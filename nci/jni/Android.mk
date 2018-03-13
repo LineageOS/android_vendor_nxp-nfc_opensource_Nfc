14fd538f74e2c5208596be24fdcf3e0016b525a6
dianlujitao <dianlujitao@lineageos.org>
2018-03-13 16:04:27 +0800

    NFC: Fix support for pn551/pn553
    Change-Id: I87595e80723fada4458a94f735b4656d18700f61

--- nci/jni/Android.mk
+++ nci/jni/Android.mk
@@ -65,8 +65,12 @@ endif
 #### Select the CHIP ####
 ifeq ($(BOARD_NFC_CHIPSET),pn547)
 NXP_CHIP_TYPE := $(PN547C2)
-else
+else ifeq ($(BOARD_NFC_CHIPSET),pn548)
 NXP_CHIP_TYPE := $(PN548C2)
+else ifeq ($(BOARD_NFC_CHIPSET),pn551)
+NXP_CHIP_TYPE := $(PN551)
+else
+NXP_CHIP_TYPE := $(PN553)
 endif
 
 ifeq ($(NXP_CHIP_TYPE),$(PN547C2))
